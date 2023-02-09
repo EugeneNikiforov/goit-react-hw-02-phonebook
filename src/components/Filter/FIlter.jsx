0ab3a79c87dcb96ea12a1e600fce9f1835109b10
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-02-09 03:21:24 +0200

    refactor

---
+++ src/components/Filter/FIlter.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Filter.module.scss';
+
+export default function Filter({ filter, change }) {
+  return (
+    <>
+      <p className={css.filter}>Find contacts by name</p>
+          <input type="text" name="found" className={css.filterFinder} value={filter} onChange={change} />
+    </>
+  );
+}
+
+Filter.protoTypes = {
+  filter: PropTypes.string.isRequired,
+  change: PropTypes.func.isRequired,
+};
